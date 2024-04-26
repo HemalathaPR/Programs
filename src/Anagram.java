12a1c5151c72b191f8d535436e92a17ca5c3e753
HemalathaPR <hemalathapr16@gmail.com>
2024-04-26 22:35:25 +0530

    firsy commit

---
+++ src/Anagram.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+/*
+@author
+ */
+public class Anagram {
+    public static void main(String[] args) {
+        String s1 = "dog";
+        String s2 = "god";
+        if (s1.length() == s2.length()) {
+            char[] ar1 = s1.toCharArray();
+            char[] ar2 = s2.toCharArray();
+            Arrays.sort(ar1);
+            Arrays.sort(ar2);
+            boolean b = Arrays.equals(ar1, ar2);
+            if (b) {
+                System.out.println("is anagram");
+            } else {
+                System.out.println("not");
+            }
+
+        }
+    }
+}
